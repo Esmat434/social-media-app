5af877fedc43338235ff056377d109cf0b9e7927
alex <alex@gmail.com>
2025-06-29 22:56:04 -0700

    addd account in INSTALED_APP and add AUTH_USER_MODEL

--- SocialMedia/settings.py
+++ SocialMedia/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -71,6 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'SocialMedia.wsgi.application'
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
