e133e981fb8751b86bd9ea84c3eea0a253b61f47
alex <alex@gmail.com>
2025-07-01 01:34:36 -0700

    add email in accounts

---
+++ accounts/email.py
@@ -0,0 +1,13 @@
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.conf import settings
+
+def send_verification_mail(to,subject,username,link):
+    html_content = render_to_string('accounts/email.html',{'username':username,'link':link})
+    email = EmailMessage(subject,html_content,settings.EMAIL_HOST_USER,[to])
+    email.content_subtype = 'html'
+
+    try:
+        email.send()
+    except Exception as e:
+        return e
