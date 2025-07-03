7b2d94320028b29e986ae24e6ca1ba5144af4f3d
alex <alex@gmail.com>
2025-07-02 20:45:36 -0700

    fix urls of accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
+from .api import urls as api_urls
+from .mvt import urls as mvt_urls
 
 urlpatterns = [
-    path('api/',include('accounts.api.urls',namespace='api')),
-    path('',include('accounts.mvt.urls',namespace='mvt'))
+    path('api/',include((api_urls,'api'),namespace='api')),
+    path('',include((mvt_urls,'mvt'),namespace='mvt'))
 ]
