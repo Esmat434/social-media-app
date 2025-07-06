62d1e0e069dcc28b9b7d7ca45d11630d35cd481e
alex <alex@gmail.com>
2025-07-05 23:08:22 -0700

    delete permessions file

--- accounts/api/permessions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class isAuthenticatedCustom(BasePermission):
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated
-    
-class isNotAuthenticatedCustom(BasePermission):
-    def has_permission(self, request, view):
-        return not (request.user and request.user.is_authenticated)
