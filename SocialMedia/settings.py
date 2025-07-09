508e01cc276058e0051548b3894f288c8f5fe77e
alex <alex@gmail.com>
2025-07-08 19:03:51 -0700

    add email settings in settings

--- SocialMedia/settings.py
+++ SocialMedia/settings.py
@@ -177,3 +177,11 @@ SIMPLE_JWT = {
 # When test is running the Axes will off
 if 'test' in sys.argv:
     AXES_ENABLED = False
+
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'hadelesmatullah@gmail.com'
+EMAIL_HOST_PASSWORD = 'cbay bqjp lnco zzac'
