74e155ea95c8131a1b0e352963cf346ff325dae0
alex <alex@gmail.com>
2025-07-13 20:30:05 -0700

    fix connections urls in connections app

--- connections/urls.py
+++ connections/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
+from connections.api import urls as api_urls
+from connections.mvt import urls as mvt_urls
 
 urlpatterns = [
-    path('api/',include('api.urls',namespace='api')),
-    path('',include('mvt.urls',namespace='mvt')),
+    path('api/',include((api_urls, 'api_connection'), namespace='api_connection')),
+    path('',include((mvt_urls, 'mvt_connection'),namespace='mvt_connection')),
 ]
