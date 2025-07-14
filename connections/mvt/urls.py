25a455521be6456a8b6d58875e1da8cdb57828f5
alex <alex@gmail.com>
2025-07-13 20:28:06 -0700

    fix mvt urls in connections app

--- connections/mvt/urls.py
+++ connections/mvt/urls.py
@@ -4,8 +4,6 @@ from .views import (
     FollowView,UnFollowView
 )
 
-app_name = 'connections'
-
 urlpatterns = [
     path('follow/<str:username>/',FollowView.as_view(),name='follow'),
     path('unfollow/<str:username>/',UnFollowView.as_view(),name='un-follow'),
