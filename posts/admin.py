ce17e0f4c3e7f1f73102cd7ebd82481f756fbadc
alex <alex@gmail.com>
2025-07-16 11:02:43 -0700

    add admins of posts app

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
+from .models import (
+    Post,Like,Comment,Share,Save
+)
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id','user','published_at','created_at','updated_at']
+    search_fields = ['id']
+    list_filter = ['user','published_at','created_at','updated_at']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id','user','post','parent','created_at']
+    search_fields = ['id']
+    list_filter = ['user','post','parent','created_at']
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['id','user','post','status','created_at']
+    search_fields = ['id']
+    list_filter = ['user','post','status','created_at']
+
+@admin.register(Share)
+class ShareAdmin(admin.ModelAdmin):
+    list_display = ['id','user','post','created_at']
+    search_fields = ['id']
+    list_filter = ['user','post','created_at']
+
+@admin.register(Save)
+class SaveAdmin(admin.ModelAdmin):
+    list_display = ['id','user','post','created_at']
+    search_fields = ['id']
+    list_filter = ['user','post','created_at']
