ebf2d26df576641e1c032f34b4a7ee2fec3b056b
alex <alex@gmail.com>
2025-07-23 22:31:31 -0700

    fix posts admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,36 +1,45 @@
 from django.contrib import admin
 
 from .models import (
-    Post,Like,Comment,Share,Save
+    Post,PostMedia,Like,Comment,Share,Save
 )
-# Register your models here.
+
+class PostMediaAdmin(admin.TabularInline):
+    model = PostMedia
+    extra = 1
+
+class CommentAdmin(admin.StackedInline):
+    model = Comment
+    fields = (
+        'user','parent','comment'
+    )
+    readonly_fields = ('user',)
+    extra = 1
+
+class LikeAdmin(admin.TabularInline):
+    model = Like
+    readonly_fields = ('user',)
+    extra = 0
+
+class ShareAdmin(admin.TabularInline):    
+    model = Share
+    readonly_fields = ('user',)
+    extra = 0
+
+class SaveAdmin(admin.TabularInline):    
+    model = Save
+    readonly_fields = ('user',)
+    extra = 0
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id','user','published_at','created_at','updated_at']
-    search_fields = ['id']
+    search_fields = ['content','user__username']
     list_filter = ['user','published_at','created_at','updated_at']
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ['id','user','post','parent','created_at']
-    search_fields = ['id']
-    list_filter = ['user','post','parent','created_at']
-
-@admin.register(Like)
-class LikeAdmin(admin.ModelAdmin):
-    list_display = ['id','user','post','status','created_at']
-    search_fields = ['id']
-    list_filter = ['user','post','status','created_at']
-
-@admin.register(Share)
-class ShareAdmin(admin.ModelAdmin):
-    list_display = ['id','user','post','created_at']
-    search_fields = ['id']
-    list_filter = ['user','post','created_at']
-
-@admin.register(Save)
-class SaveAdmin(admin.ModelAdmin):
-    list_display = ['id','user','post','created_at']
-    search_fields = ['id']
-    list_filter = ['user','post','created_at']
+    inlines = (
+        PostMediaAdmin,
+        CommentAdmin,
+        LikeAdmin,
+        ShareAdmin,
+        SaveAdmin
+    )
