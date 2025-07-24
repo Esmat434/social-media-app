8eb276442c3be7b5991da14ffba3440ba8830a71
alex <alex@gmail.com>
2025-07-23 22:34:27 -0700

    add posts urls in main urls

--- SocialMedia/urls.py
+++ SocialMedia/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('accounts.urls')),
     path('',include('connections.urls')),
+    path('',include('posts.urls')),
 ]
 
 if settings.DEBUG:
