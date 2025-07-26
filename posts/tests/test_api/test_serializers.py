5010c6ca10f4de78e0927698ceda072c55915f0b
alex <alex@gmail.com>
2025-07-26 09:22:34 -0700

    fix test_serializer in posts app

--- posts/tests/test_api/test_serializers.py
+++ posts/tests/test_api/test_serializers.py
@@ -28,14 +28,31 @@ class TestSerializers:
         self.user = user
         self.post = Post.objects.create(user=self.user, content='test content')
 
-    def test_post_serializer(self):
-        data = {
+    def test_post_serializer_validate_data(self):
+        valid_data = {
             'content':'test content.'
         }
+
+        serializer = PostSerializer(data=valid_data)
+
+        assert serializer.is_valid(raise_exception=True)
+
+        post_instance = serializer.save(user=self.user)
+
+        assert post_instance.user == self.user
+        assert post_instance.content == 'test content.'
+    
+    def test_post_serializer_invalid_data(self):
+        data = {
+            'content':'this is some fuck word'
+        }
+
         serializer = PostSerializer(data=data)
 
-        assert serializer.is_valid() == True
-    
+        assert not serializer.is_valid()
+        
+        assert 'content' in serializer.errors
+
     def test_post_media_serializer(self):
         fake_file = SimpleUploadedFile(
             name='test.jpeg',
@@ -50,15 +67,34 @@ class TestSerializers:
 
         assert serializer.is_valid() == True
     
-    def test_comment_serializer(self):
-        data = {
+    def test_comment_serializer_validate_data(self):
+        valid_data = {
             'post':self.post.id,
             'parent':None,
             'comment':'this is comment'
         }
+
+        valid_serializer = CommentSerializer(data=valid_data)
+
+        assert valid_serializer.is_valid(), valid_serializer.errors
+
+        comment_instance = valid_serializer.save(user=self.user)
+
+        assert comment_instance.user == self.user
+        assert comment_instance.comment == 'this is comment'
+
+    def test_comment_serializer_invalid_data(self):
+        data = {
+            'post':self.post.pk,
+            'parent':None,
+            'comment':'this is fuck comment'
+        }
+
         serializer = CommentSerializer(data=data)
-        
-        assert serializer.is_valid() == True
+
+        assert not serializer.is_valid()
+
+        assert 'comment' in serializer.errors
 
     def test_like_serializer(self):
         data = {
