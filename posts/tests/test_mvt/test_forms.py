9bd9375bd56d9c3428f0fab89c7be92610f38452
alex <alex@gmail.com>
2025-07-26 19:23:55 -0700

    fix test_forms in posts app

--- posts/tests/test_mvt/test_forms.py
+++ posts/tests/test_mvt/test_forms.py
@@ -23,10 +23,11 @@ class TestPostForm:
         assert form.is_valid() == False
 
 class TestPostMediaForm:
-    def test_post_media_form_validate(self):
+    def test_post_media_form_validate_data(self):
+        large_content = b'a' * (4 * 1024 * 1024)
         fake_file = SimpleUploadedFile(
             name='test.jpeg',
-            content=b'\x47\x49\x46',
+            content=large_content,
             content_type='image/jpeg'
         )
 
@@ -36,6 +37,20 @@ class TestPostMediaForm:
 
         assert form.is_valid() == True
     
+    def test_post_media_form_invalid_data(self):
+        large_content = b'a' * (50 * 1024 * 1024)
+        fake_file = SimpleUploadedFile(
+            name='test.jpeg',
+            content=large_content,
+            content_type='image/jpeg'
+        )
+
+        files = {'file':fake_file}
+
+        form = PostMediaForm(files=files)
+
+        assert form.is_valid() == False
+
     def test_post_media_form_invalid_file_type(self):
         fake_file = SimpleUploadedFile(
             name='test.hdi',
