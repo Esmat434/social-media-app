fe8cf61b0faef703a9efa940e4c3a410173b2290
alex <alex@gmail.com>
2025-07-29 11:22:46 -0700

    fix api urls in connections app

--- connections/api/urls.py
+++ connections/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from .views import (
-    FollowToggleAPIView
+    FollowToggleAPIView,UserPrivateToggleAPIView
 )
 
 urlpatterns = [
     path('follow/<str:username>/',FollowToggleAPIView.as_view(),name='follow'),
+    path('connection/request/<str:username>/',UserPrivateToggleAPIView.as_view(),name='connection-request'),
 ]
