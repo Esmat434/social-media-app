4b2bdbaa4fd17a3d93badaaec27de78b9289c731
alex <alex@gmail.com>
2025-07-28 21:49:28 -0700

    add url of mvt in connections app

--- connections/mvt/urls.py
+++ connections/mvt/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from .views import (
-    FollowView,UnFollowView
+    FollowView,UserPrivateRequestView,UserPrivateAcceptRequestView,UserPrivateRejectRequestView,
+    UnFollowView
 )
 
 urlpatterns = [
     path('follow/<str:username>/',FollowView.as_view(),name='follow'),
+    path('follow/<str:username>/private_request/',UserPrivateRequestView.as_view(),name='follow-private-request'),
+    path('follow/<str:username>/private_accept/',UserPrivateAcceptRequestView.as_view(),name='follow-private-accept'),
+    path('follow/<str:username>/private_reject/',UserPrivateRejectRequestView.as_view(),name='follow-private-reject'),
     path('unfollow/<str:username>/',UnFollowView.as_view(),name='un-follow'),
 ]
