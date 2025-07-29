346b6db6205920e4bdcdea3e38fe685cf49ac700
alex <alex@gmail.com>
2025-07-28 21:47:16 -0700

    add test_views in connections app

--- connections/tests/mvt/test_views.py
+++ connections/tests/mvt/test_views.py
@@ -19,32 +19,75 @@ def to_user(db):
     return User.objects.create_user(username='test2',email='test2@gmail.com',address='test2',
                                     city='test2',country='AF')
 
+@pytest.fixture
+def auth_client(from_user,client):
+    client.force_login(from_user)
+    return client
+
 @pytest.mark.django_db
 class TestFollowView:
     @pytest.fixture(autouse=True)
-    def setUp(self,from_user,to_user,client):
-        self.from_user = from_user
+    def setUp(self,to_user,auth_client):
         self.to_user = to_user
-        self.client = client
+        self.client = auth_client
     
     def test_follow_view(self):
-        self.client.force_login(self.from_user)
         url = reverse('mvt_connection:follow', args=[self.to_user.username])
         response = self.client.post(url)
 
         assert response.status_code == 201
 
 @pytest.mark.django_db
+class TestUserPrivateRequestView:
+    @pytest.fixture(autouse=True)
+    def setUp(self,to_user,auth_client):
+        self.to_user=to_user
+        self.client=auth_client
+    
+    def test_post_method(self):
+        url = reverse('mvt_connection:follow-private-request', args=[self.to_user.username])
+        response = self.client.post(url)
+
+        assert response.status_code == 201
+
+@pytest.mark.django_db
+class TestUserPrivateAcceptedRequestView:
+    @pytest.fixture(autouse=True)
+    def setUp(self,from_user,to_user,auth_client):
+        self.to_user=to_user
+        self.client=auth_client
+        self.connection = Connection.objects.create(from_user=to_user, to_user=from_user, status=Connection.ConnectionStatus.PENDING)
+
+    def test_post_method(self):
+        url = reverse('mvt_connection:follow-private-accept', args=[self.to_user.username])
+        response = self.client.post(url)
+
+        assert response.status_code == 200
+
+@pytest.mark.django_db
+class TestUserPrivateRejectedRequestView:
+    @pytest.fixture(autouse=True)
+    def setUp(self,from_user,to_user,auth_client):
+        self.to_user = to_user
+        self.client = auth_client
+        self.connection = Connection.objects.create(from_user=to_user, to_user=from_user)
+    
+    def test_post_method(self):
+        url = reverse('mvt_connection:follow-private-reject', args=[self.to_user.username])
+        response = self.client.post(url)
+
+        assert response.status_code == 200
+
+@pytest.mark.django_db
 class TestUnFollowView:
     @pytest.fixture(autouse=True)
-    def setUp(self,from_user,to_user,client):
+    def setUp(self,from_user,to_user,auth_client):
         self.from_user = from_user
         self.to_user = to_user
-        self.client = client
-        self.connection = Connection.objects.create(from_user=self.from_user, to_user=self.to_user)
+        self.client = auth_client
+        self.connection = Connection.objects.create(from_user=self.from_user, to_user=self.to_user, status=Connection.ConnectionStatus.ACCEPTED)
     
     def test_unfollow_view(self):
-        self.client.force_login(self.from_user)
         url = reverse('mvt_connection:un-follow', args=[self.to_user.username])
         response = self.client.post(url)
 
