31ac23960259bdce0d969d5afd53a45f2667eabb
alex <alex@gmail.com>
2025-07-28 21:46:08 -0700

    add test_urls in connections app

--- connections/tests/mvt/test_urls.py
+++ connections/tests/mvt/test_urls.py
@@ -4,6 +4,14 @@ def test_follow_url():
     url = reverse('mvt_connection:follow', args=['testuser'])
     assert url == '/follow/testuser/'
 
+def test_follow_private_request():
+    url = reverse('mvt_connection:follow-private-request', args=['testuser'])
+    assert url == '/follow/testuser/private_request/'
+
+def test_follow_private_accept():
+    url = reverse('mvt_connection:follow-private-accept', args=['testuser'])
+    assert url == '/follow/testuser/private_accept/'
+
 def test_unfollow_url():
     url = reverse('mvt_connection:un-follow', args=['testuser'])
     assert url == '/unfollow/testuser/'
