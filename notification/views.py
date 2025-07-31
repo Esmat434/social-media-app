fa00fd8ea498d259e6a94de4f88ea54839e51f79
alex <alex@gmail.com>
2025-07-31 09:20:22 -0700

    add DeleteNotificationView in view in notification app

--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from django.views import View
 
+from .models import Notification
 # Create your views here.
+
+class DeleteNotificatoinView(View):
+    def post(self,request, pk):
+        notification = get_object_or_404(
+            Notification, 
+            id=pk,
+            recipient = request.user
+        )
+
+        target_url = None
+        if notification.target and hasattr(notification.target, 'get_absolute_url'):
+            target_url = notification.target.get_absolute_url()
+        
+        notification.delete()
+
+        if target_url:
+            return redirect(target_url)
+        
+        return redirect('home:home-feed')
+    
