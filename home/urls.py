1c72d52b1ee13e401ca28878ad19561b78fc1a17
alex <alex@gmail.com>
2025-08-01 04:07:32 -0700

    add urls in home app

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from .views import (
-    PostListView,NetworkListView
+    PostListView,NetworkListView,NotificationListView,SaveListView
 )
 
 app_name = 'home'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home-feed'),
+    path('networks/', NetworkListView.as_view(), name='networks'),
+    path('notifications/', NotificationListView.as_view(), name='notifications'),
+    path('saves/', SaveListView.as_view(), name='saves/')
 ]
