bcc47383e54a406280037b0573a72c6174e5cad5
alex <alex@gmail.com>
2025-07-31 22:04:27 -0700

    fix notification views

--- notification/views.py
+++ notification/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404,redirect
+from django.shortcuts import get_object_or_404,redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 
