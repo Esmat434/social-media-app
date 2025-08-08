acfa7800d4540a431db5b1476269d195ca05ab7e
alex <alex@gmail.com>
2025-08-08 10:54:57 -0700

    fix mvt test_urls in search app

--- search/tests/test_mvt/test_urls.py
+++ search/tests/test_mvt/test_urls.py
@@ -2,6 +2,16 @@ from django.urls import reverse
 
 class TestUrls:
     def test_post_search_url(self):
-        url = reverse('post_search')+'?q=hi'
+        url = reverse('mvt_search:post_search')+'?q=hi'
 
-        assert url == '/search/post/?p=hi'
+        assert url == '/search/post/?q=hi'
+    
+    def test_network_search_url(self):
+        url = reverse('mvt_search:network_search')+'?q=hi'
+
+        assert url == '/search/network/?q=hi'
+    
+    def test_notification_search_url(self):
+        url = reverse('mvt_search:notification_search')+'?q=hi'
+
+        assert url == '/search/notification/?q=hi'
