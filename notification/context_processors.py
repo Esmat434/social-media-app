c730472e1936902d79039d7d8e0252817fea91d8
alex <alex@gmail.com>
2025-08-08 00:56:37 -0700

    add context_processros in notification app

---
+++ notification/context_processors.py
@@ -0,0 +1,13 @@
+from .models import Notification
+
+def notification_count(request):
+    if request.user.is_authenticated:
+        count = Notification.objects.filter(
+            recipient=request.user,
+        ).count()
+    else:
+        count = 0
+    
+    return {
+        'notification_count':count
+    }
