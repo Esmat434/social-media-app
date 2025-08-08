2e13a81892f37e0feb78407e598d4973f25e147e
alex <alex@gmail.com>
2025-08-08 05:43:07 -0700

    fix mvt urls in search app

--- search/mvt/urls.py
+++ search/mvt/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 from .views import (
-    SearchPostView
+    SearchPostView,SearchNetworkView,SearchNotificationView
 )
+
+app_name = 'mvt_search'
 
 urlpatterns = [
     path('post/', SearchPostView.as_view(), name='post_search'),
+    path('network/', SearchNetworkView.as_view(), name='network_search'),
+    path('notification/', SearchNotificationView.as_view(), name='notification_search'),
 ]
