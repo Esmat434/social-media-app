2ffa6351e85025fcc1b5af1d98a6bc2a62996a36
alex <alex@gmail.com>
2025-08-08 01:11:34 -0700

    fix notification context_processros in settings

--- SocialMedia/settings.py
+++ SocialMedia/settings.py
@@ -77,7 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'notification.context_processros.notification_count',
+                'notification.context_processors.notification_count',
             ],
         },
     },
