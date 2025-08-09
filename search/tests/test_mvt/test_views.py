c63b22aa1e1d7e0ef6d1b887d5ecc1271087ba88
alex <alex@gmail.com>
2025-08-08 20:42:57 -0700

    add another mvt TestSearch Views in search app

--- search/tests/test_mvt/test_views.py
+++ search/tests/test_mvt/test_views.py
@@ -1,10 +1,24 @@
 from django.urls import reverse
 import pytest
 
-@pytest.mark.django_db
+
 class TestSearchPostView:
     def test_get_method(self,client):
         url = reverse('mvt_search:post_search')+'?q=hello'
+        response = client.get(url)
+
+        assert response.status_code == 200
+
+class TestSearchNetworkView:
+    def test_get_method(self,client):
+        url =  reverse('mvt_search:network_search')+'?q=jan'
+        response = client.get(url)
+
+        assert response.status_code == 200
+
+class TestSearchNotificationView:
+    def test_get_method(self,client):
+        url = reverse('mvt_search:notification_search')+'?q=liked'
         response = client.get(url)
 
         assert response.status_code == 200
