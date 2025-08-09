24d49cfc41bcd476fbfe89cb9c29f358aa0ae0f3
alex <alex@gmail.com>
2025-08-08 22:37:51 -0700

    add urls in search app

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include
 
 from search.mvt import urls as mvt_urls
+from search.api import urls as api_urls
 
 urlpatterns = [
-    path('search/', include((mvt_urls, 'mvt_search'), namespace='mvt_search'))
+    path('search/', include((mvt_urls, 'mvt_search'), namespace='mvt_search')),
+    path('api/search/', include((api_urls, 'api_search'), namespace='api_search')),
 ]
