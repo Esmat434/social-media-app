9178240768a0b3ef5a0453a7f632784f6067b47d
alex <alex@gmail.com>
2025-08-08 20:16:06 -0700

    add watson in apps in notification app

--- notification/apps.py
+++ notification/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class NotificationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "notification"
+
+    def ready(self):
+        from watson import search as watson
+        Notification = self.get_model('Notification')
+        watson.register(Notification, fields=('verb',))
