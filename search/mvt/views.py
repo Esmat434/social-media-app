11fb8f71d478098daeca67b477ef4ae37d0ca3cd
alex <alex@gmail.com>
2025-08-09 09:34:16 -0700

    fix mvt views in search app

--- search/mvt/views.py
+++ search/mvt/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.views import View
 from django.contrib.auth import get_user_model
 from watson import search as watson_search
+from itertools import chain
 
 from posts.models import (
     Post
+)
+
+from connections.models import (
+    Connection
 )
 
 from notification.models import (
@@ -30,29 +36,83 @@ class SearchPostView(View):
 
 class SearchNetworkView(View):
     def get(self,request):
-        query = request.GET.get('q', '')
-        results = []
-        if query:
-            user_results = watson_search.filter(User,query)
-            results = user_results
-        print(query)
+        
         context = {
-            'other_network_list': results,
+            'other_network_list': self.get_search_results(request),
             'near_network_list': []
         }
 
         return render(request,'home/network.html', context=context)
 
+    def get_connected_users_id(self,request):
+        if not request.user.is_authenticated:
+            return [] 
+        
+        connections = Connection.objects.filter(
+            (Q(from_user=request.user) | Q(to_user=request.user)),
+            (Q(status=Connection.ConnectionStatus.ACCEPTED) | Q(status=Connection.ConnectionStatus.PENDING))
+        )
+        
+        connected_ids = set()
+        for conn in connections:
+            if conn.from_user_id != request.user.id:
+                connected_ids.add(conn.from_user_id)
+            if conn.to_user_id != request.user.id:
+                connected_ids.add(conn.to_user_id)
+        
+        return list(connected_ids)
+    
+    def get_search_results(self,request):
+        query = request.GET.get('q', '')
+        if not query:
+            return []
+
+        connected_user_ids = self.get_connected_users_id(request)
+        
+        # همیشه ID کاربر فعلی را هم به لیست استثنائات اضافه کن
+        all_excluded_ids = set(connected_user_ids)
+        if request.user.is_authenticated:
+            all_excluded_ids.add(request.user.id)
+        
+        watson_results = watson_search.filter(User,query)
+        watson_results=watson_results.exclude(id__in=connected_user_ids)
+        watson_results_ids = [result.id for result in watson_results]
+        all_excluded_ids = list(all_excluded_ids)+watson_results_ids
+
+        partial_results = User.objects.filter(
+            Q(username__icontains=query) | 
+            Q(first_name__icontains=query) |
+            Q(last_name__icontains=query)
+        ).exclude(
+            id__in=all_excluded_ids
+        )
+
+        results = list(chain(watson_results, partial_results))
+
+        return results            
+
 class SearchNotificationView(View):
     def get(self,request):
         query = request.GET.get('q', '')
-        results = []
-        if query:
-            notification_results = watson_search.filter(Notification,query)
-            results = notification_results
+        my_notification_ids = self.get_my_notification_ids(request)
+        if not query:
+            return []
+        
+        notification_results = watson_search.filter(Notification,query)
+        results = notification_results.filter(id__in=my_notification_ids)
         
         context = {
             'notifications': results
         }
 
         return render(request,'home/notification.html', context=context)
+    
+    def get_my_notification_ids(self,request):
+        if not request.user.is_authenticated:
+            return []
+        
+        notification_ids = Notification.objects.filter(
+            recipient=request.user
+        ).values_list('id', flat=True)
+
+        return notification_ids
