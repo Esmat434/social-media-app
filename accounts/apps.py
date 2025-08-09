efc10f5301ef726e493079ce29640a6c8b44afa2
alex <alex@gmail.com>
2025-08-08 20:25:21 -0700

    add watson in apps in accounts app

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        from watson import search as watson
+        User = self.get_model('CustomUser')
+        watson.register(User, fields=('verb',))
