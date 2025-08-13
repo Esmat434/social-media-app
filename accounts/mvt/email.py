b3140f492342ae530c9062369979034812f4eb7f
alex <alex@gmail.com>
2025-08-13 05:46:30 -0700

    fix email in accounts

--- accounts/mvt/email.py
+++ accounts/mvt/email.py
@@ -2,7 +2,7 @@ from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.conf import settings
 
-def send_verification_mail(to,subject,username,link):
+def send_verification_mail_async(to,subject,username,link):
     html_content = render_to_string('accounts/email.html',{'username':username,'link':link})
     email = EmailMessage(subject,html_content,settings.EMAIL_HOST_USER,[to])
     email.content_subtype = 'html'
