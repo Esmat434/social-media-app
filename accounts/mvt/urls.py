ca56e5de4468fb5395bf5619297f4263880ca9a7
alex <alex@gmail.com>
2025-08-13 11:22:20 -0700

    add friendlist view url in mvt urls in accounts app

--- accounts/mvt/urls.py
+++ accounts/mvt/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import (
-    RegisterView,LoginView,LogoutView,ProfileView,ProfileUpdateView,
+    RegisterView,LoginView,LogoutView,ProfileView,FriendListView,ProfileUpdateView,
     CreateAccountVerifiedTokenView,AccountVerifiedView,
     CreateChangePasswordTokenView,ChangePasswordView,CreateForgotPasswordTokenView,
     ForgotPasswordView
@@ -13,6 +13,7 @@ urlpatterns = [
     path('login/',LoginView.as_view(),name='login'),
     path('logout/',LogoutView.as_view(),name='logout'),
     path('profile/<str:username>/',ProfileView.as_view(),name='profile'),
+    path('friends/',FriendListView.as_view(), name='friends'),
     path('profile_update/',ProfileUpdateView.as_view(),name='profile_update'),
     path('create_account_verify_token/',CreateAccountVerifiedTokenView.as_view(),name='create_account_verify_token'),
     path('account_verified/<uuid:uuid>/',AccountVerifiedView.as_view(),name='account_verified'),
