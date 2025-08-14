2f2c3461f46db2c9ac4c5d7554e83570571da076
alex <alex@gmail.com>
2025-08-13 21:58:04 -0700

    add api test search friends view in search app

--- search/tests/test_api/test_views.py
+++ search/tests/test_api/test_views.py
@@ -1,26 +1,61 @@
 import pytest
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
+
+@pytest.fixture
+def user(db):
+    password = 'Test12345%'
+    user = User.objects.create_user(
+            username='test',email='test@gmail.com',address='Timany',city='Kabul',country='AF',
+            email_verified=True,password=password
+            )
+    user.raw_password = password
+    return user
+
+@pytest.fixture
+def auth_client(user,client):
+    refresh = RefreshToken.for_user(user)
+    access_token = str(refresh.access_token)
+    client.defaults['HTTP_AUTHORIZATION'] = f'Bearer {access_token}'
+    return client
+
+class SetUp:
+    @pytest.fixture(autouse=True)
+    def setUp(self, auth_client):
+        self.client = auth_client
 
 @pytest.mark.django_db
-class TestSearchPostView:
-    def test_get_method(self,client):
+class TestSearchPostView(SetUp):
+    def test_get_method(self):
         url = reverse('api_search:post_search')+'?q=test'
-        response = client.get(url)
+        response = self.client.get(url)
 
         assert response.status_code == 200
 
 @pytest.mark.django_db
-class TestSearchUserView:
-    def test_get_method(self,client):
+class TestSearchUserView(SetUp):
+    def test_get_method(self):
         url = reverse('api_search:user_search')+'?q=test'
-        response = client.get(url)
+        response = self.client.get(url)
 
         assert response.status_code == 200
 
 @pytest.mark.django_db
-class TestSearchPostSaveView:
-    def test_get_method(self,client):
+class TestSearchPostSaveView(SetUp):
+    def test_get_method(self):
         url = reverse('api_search:post_save_search')+'?q=test'
-        response = client.get(url)
+        response = self.client.get(url)
+
+        assert response.status_code == 200
+
+@pytest.mark.django_db
+class testSearchFriendView(SetUp):
+    def test_get_method(self):
+        url = reverse('api_search:friend_search')+'?q=test'
+        response = self.client.get(url)
 
         assert response.status_code == 200
