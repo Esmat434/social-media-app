23397debe3946b551048150c4e664bbfa7c69c8d
alex <alex@gmail.com>
2025-08-13 21:21:32 -0700

    add searchfriend view in mvt url in search app

--- search/mvt/urls.py
+++ search/mvt/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import (
-    SearchPostView,SearchNetworkView,SearchNotificationView,SearchPostSaveView
+    SearchPostView,SearchNetworkView,SearchNotificationView,SearchPostSaveView,SearchFriendView
 )
 
 app_name = 'mvt_search'
@@ -10,5 +10,6 @@ urlpatterns = [
     path('post/', SearchPostView.as_view(), name='post_search'),
     path('network/', SearchNetworkView.as_view(), name='network_search'),
     path('notification/', SearchNotificationView.as_view(), name='notification_search'),
-    path('saves/', SearchPostSaveView.as_view(), name='post_save_search')
+    path('saves/', SearchPostSaveView.as_view(), name='post_save_search'),
+    path('friends/', SearchFriendView.as_view(), name='friend_search'),
 ]
