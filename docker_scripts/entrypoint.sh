ef24aa0b563d7209eef4c827d8144fc3df370d8d
alex <alex@gmail.com>
2025-08-18 22:22:42 -0700

    fix entrypoint.sh

--- docker_scripts/entrypoint.sh
+++ docker_scripts/entrypoint.sh
@@ -8,10 +8,4 @@ python manage.py migrate --noinput
 echo "🧱 اجرای collectstatic..."
 python manage.py collectstatic --noinput
 
-if [ "$DJANGO_ENV" = "production" ]; then
-    echo "🚀 اجرای سرور در حالت Production با Gunicorn"
-    exec gunicorn SocialMedia.wsgi:application --bind 0.0.0.0:8000
-else
-    echo "🚀 اجرای سرور در حالت Development با runserver"
-    exec python manage.py runserver 0.0.0.0:8000
-fi
+exec "$@"
