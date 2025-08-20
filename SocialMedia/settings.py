43d68c6f25c683a74830a0b5ac0679a7a32846ac
alex <alex@gmail.com>
2025-08-20 08:51:58 -0700

    fix settings

--- SocialMedia/settings.py
+++ SocialMedia/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS',"127.0.0.1").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost:8080').split(",")
 
 
 # Application definition
